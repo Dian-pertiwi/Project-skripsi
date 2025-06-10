c4c06150ee9d48a47ec479fcfc881354b3bce523
Dian-pertiwi <dianpertiwidian02@gmail.com>
2025-06-10 21:53:47 +0800

    up

--- admin-kurir/detail_order.php
+++ admin-kurir/detail_order.php
@@ -2,7 +2,7 @@
 include 'config/koneksi.php';
 $id_order = $_GET['id'] ?? 0;
 
-$query = $conn->prepare("SELECT * FROM tbl_order_masuk WHERE id_order_masuk = ?");
+$query = $conn->prepare("SELECT * FROM tbl_pengiriman WHERE id_pengiriman = ?");
 $query->bind_param("i", $id_order);
 $query->execute();
 $result = $query->get_result();
@@ -12,6 +12,27 @@ if ($result->num_rows === 0) {
 }
 
 $order = $result->fetch_assoc();
+
+// Ambil data pengirim
+$query_pengirim = $conn->prepare("SELECT * FROM tbl_pengirim WHERE id_pengirim = ?");
+$query_pengirim->bind_param("i", $order['id_pengirim']);
+$query_pengirim->execute();
+$result_pengirim = $query_pengirim->get_result();
+$pengirim = $result_pengirim->fetch_assoc();
+
+// Ambil data penerima
+$query_penerima = $conn->prepare("SELECT * FROM tbl_penerima WHERE id_penerima = ?");
+$query_penerima->bind_param("i", $order['id_penerima']);
+$query_penerima->execute();
+$result_penerima = $query_penerima->get_result();
+$penerima = $result_penerima->fetch_assoc();
+
+// Ambil data barang
+$query_barang = $conn->prepare("SELECT * FROM tbl_barang WHERE id_barang = ?");
+$query_barang->bind_param("i", $order['id_barang']);
+$query_barang->execute();
+$result_barang = $query_barang->get_result();
+$barang = $result_barang->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +54,7 @@ $order = $result->fetch_assoc();
                     <div class="card shadow mb-4">
                         <div class="card-header py-3 bg-primary text-white">
                             <h6 class="m-0 font-weight-bold">Informasi Orderan
-                                #<?= htmlspecialchars($order['kode_order'] ?? 'PKT' . str_pad($order['id_order_masuk'], 3, '0', STR_PAD_LEFT)) ?>
+                                #<?= htmlspecialchars($order['kode_pengiriman'] ?? 'PKT' . str_pad($order['id_pengiriman'], 3, '0', STR_PAD_LEFT)) ?>
                             </h6>
                         </div>
                         <div class="card-body">
@@ -49,27 +70,27 @@ $order = $result->fetch_assoc();
                                     <tbody>
                                         <tr>
                                             <th>Nama</th>
-                                            <td><?= htmlspecialchars($order['nama_pengirim']) ?></td>
+                                            <td><?= htmlspecialchars($pengirim['nama_pengirim']) ?></td>
                                         </tr>
                                         <tr>
                                             <th>Kabupaten</th>
-                                            <td><?= htmlspecialchars($order['kabupaten_pengirim']) ?></td>
+                                            <td><?= htmlspecialchars($pengirim['kabupaten_pengirim']) ?></td>
                                         </tr>
                                         <tr>
                                             <th>Kecamatan</th>
-                                            <td><?= htmlspecialchars($order['kecamatan_pengirim']) ?></td>
+                                            <td><?= htmlspecialchars($pengirim['kecamatan_pengirim']) ?></td>
                                         </tr>
                                         <tr>
                                             <th>Alamat Lengkap</th>
-                                            <td><?= nl2br(htmlspecialchars($order['alamat_pengirim'])) ?></td>
+                                            <td><?= nl2br(htmlspecialchars($pengirim['alamat_pengirim'])) ?></td>
                                         </tr>
                                         <tr>
                                             <th>No. HP</th>
-                                            <td><?= htmlspecialchars($order['hp_pengirim']) ?></td>
+                                            <td><?= htmlspecialchars($pengirim['hp_pengirim']) ?></td>
                                         </tr>
                                         <tr>
                                             <th>Nama Bank</th>
-                                            <td><?= htmlspecialchars($order['bank_pengirim']) . ' - ' . htmlspecialchars($order['no_rekening']) ?>
+                                            <td><?= htmlspecialchars($pengirim['bank_pengirim']) . ' - ' . htmlspecialchars($pengirim['no_rekening']) ?>
                                             </td>
                                         </tr>
                                     </tbody>
@@ -87,27 +108,27 @@ $order = $result->fetch_assoc();
                                     <tbody>
                                         <tr>
                                             <th>Nama</th>
-                                            <td><?= htmlspecialchars($order['nama_penerima']) ?></td>
+                                            <td><?= htmlspecialchars($penerima['nama_penerima']) ?></td>
                                         </tr>
                                         <tr>
                                             <th>Kabupaten</th>
-                                            <td><?= htmlspecialchars($order['kabupaten_penerima']) ?></td>
+                                            <td><?= htmlspecialchars($penerima['kabupaten_penerima']) ?></td>
                                         </tr>
                                         <tr>
                                             <th>Kecamatan</th>
-                                            <td><?= htmlspecialchars($order['kecamatan_penerima']) ?></td>
+                                            <td><?= htmlspecialchars($penerima['kecamatan_penerima']) ?></td>
                                         </tr>
                                         <tr>
                                             <th>Alamat Lengkap</th>
-                                            <td><?= nl2br(htmlspecialchars($order['alamat_penerima'])) ?></td>
+                                            <td><?= nl2br(htmlspecialchars($penerima['alamat_penerima'])) ?></td>
                                         </tr>
                                         <tr>
                                             <th>No. HP</th>
-                                            <td><?= htmlspecialchars($order['hp_penerima']) ?></td>
+                                            <td><?= htmlspecialchars($penerima['hp_penerima']) ?></td>
                                         </tr>
                                         <tr>
                                             <th>Link Maps</th>
-                                            <td><a href="<?= htmlspecialchars($order['link_maps']) ?>"
+                                            <td><a href="<?= htmlspecialchars($penerima['link_maps']) ?>"
                                                     target="_blank">Lihat Lokasi</a></td>
                                         </tr>
                                     </tbody>
@@ -125,15 +146,15 @@ $order = $result->fetch_assoc();
                                     <tbody>
                                         <tr>
                                             <th>Jenis Barang</th>
-                                            <td><?= htmlspecialchars($order['jenis_barang']) ?></td>
+                                            <td><?= htmlspecialchars($barang['jenis_barang']) ?></td>
                                         </tr>
                                         <tr>
                                             <th>Berat Barang</th>
-                                            <td><?= htmlspecialchars($order['berat_barang']) ?> kg</td>
+                                            <td><?= htmlspecialchars($barang['berat_barang']) ?> kg</td>
                                         </tr>
                                         <tr>
                                             <th>Harga Barang</th>
-                                            <td>Rp <?= number_format($order['harga_barang'], 0, ',', '.') ?></td>
+                                            <td>Rp <?= number_format($barang['harga_barang'], 0, ',', '.') ?></td>
                                         </tr>
                                         <tr>
                                             <th>Status Pembayaran</th>
